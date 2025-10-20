7c95d78b81f9f41924620f1311f66372d9a91a38
jhomyr <jpalmera@addu.edu.ph>
2025-10-20 20:53:43 +0800

    Create TaskManager class with start method
    Fix class name and welcome message syntax.

---
+++ TaskManager.java
@@ -0,0 +1,14 @@
+package com.mycompany.dsa_final_project;
+
+import java.util.Scanner;
+
+public class TaksManager(){
+    public TaskManager(){
+    }
+
+
+    public void start(Scanner scanner){
+    System.out.print("Welcome to the main menu)";    
+  }
+}
+  
