2aec6ad8023a73e91610bbfd3f131e7fb20f30c4
jhomyr <jpalmera@addu.edu.ph>
2025-10-22 19:52:21 +0800

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -3,12 +3,37 @@ package com.mycompany.dsa_final_project;
 import java.util.Scanner;
 
 public class TaksManager(){
+    private LinkedList taskList;
+    
+    
     public TaskManager(){
+        this.taskList = new LInkedList();
     }
 
 
     public void start(Scanner scanner){
-    System.out.print("Welcome to the main menu)";    
+    int choice;    
   }
+     private void displayMenu() {
+
+    }
+    private void addTask(Scanner scanner) {
+
+    }
+    private void ViewAllTask(){
+
+    }
+    private void ViewFirstTask(){
+
+    }
+    private void deleteTask(Scanner scanner) {
+
+    }
+    private void MarkTaskComplete(Scanner scanner) {
+
+    }
+    private void showStatistics() {
+
+    } 
 }
   
