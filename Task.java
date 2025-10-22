f69a3ef3ff01e03c9fca7a633e2c87e2d06b9f2e
jhomyr <jpalmera@addu.edu.ph>
2025-10-22 19:08:16 +0800

    Add constructors and toString method to Task class
    Added constructors for Task class with different parameters and implemented toString method.

--- Task.java
+++ Task.java
@@ -1,1 +1,31 @@
+package com.mycompany.dsa_final_project;
 
+import java.util.Scanner;
+
+public class Task{
+  public String name; //initiation for the string so we can just make a constructor for specific paremeters
+  public String priority;
+  public String category;
+  public Boolean completed;
+}
+
+public class Task(String name, String priority, String category){ // constructor for this specific paremeter where we will put the main menu
+  this.name = name;
+  this.priority = priority;
+  this.category = category;
+}
+public class Task(String name, String priority, String category, Boolean completed){ // constructor with different paremeter 
+  this.name = name;
+  this.priority = priority;
+  this.category = category;
+  this.completed = completed;
+}
+
+@override
+  public String toString(){
+    String status = completed ? "Done" : "Pending";
+      return "[" + status + "] " + name
+             + " | Priority: " + priority
+             + " | Category: " + category;
+    }
+}
