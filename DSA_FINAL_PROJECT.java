8dca72ac61755d644eb8ebc311cc7f1d84ed892a
jhomyr <jpalmera@addu.edu.ph>
2025-10-28 21:57:38 +0800

    Refactor DSA_FINAL_PROJECT class structure
    Refactor main class and improve comments

--- DSA_FINAL_PROJECT.java
+++ DSA_FINAL_PROJECT.java
@@ -1,13 +1,17 @@
-package com.mycompany.dsa_final_project;
-import java.util.Scanner;
+package com.mycompany.dsa_final_project; //package to put in all the classes
 
-public class DSA_Final_Porject{
- public static void main(String[]args){
-       Scanner scanner = new Scanner(System.in);
-       TaskManager taskManager = new TaskManager();
-       System.out.print("Welcome to the Task Manager");
+import java.util.Scanner; //import so we can use user input
 
-    taskManager.start(scanner);
-    scanner.close();
-}
+public class DSA_FINAL_PROJECT {        //this is the entry class point that contains the main class
+    public static void main(String[]args){  //where it starts running
+
+        Scanner scanner = new Scanner(System.in); // creating "scanner" to take user input
+        
+        TaskManager taskManager = new TaskManager(); // calling the TaskManager to taskManager 
+    
+        System.out.println("Welcome to the Task Manager System"); //print out 
+    
+    taskManager.start(scanner); //this just callthat taskManager then getting the user input as choice
+    scanner.close();// to exit the program
+    }
 }
