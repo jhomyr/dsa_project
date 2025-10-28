0826112dc23a28142446030e1ae13f1447756e81
jhomyr <jpalmera@addu.edu.ph>
2025-10-28 21:57:22 +0800

    Refactor Node class comments for better clarity
    Updated comments for clarity and consistency in Node class.

--- Node.java
+++ Node.java
@@ -1,13 +1,16 @@
 package com.mycompany.dsa_final_project;
 
-public class Node<T>{
-  public T data; // where we store the data think of it as a box
-  public Node<T> next; // where we put the reference to the next node of the chain
-  public Node<T> previous; // where we put the reference pointer for the previous node of the chain 
 
-public Node(T data){ //constructor so we can use this format to store something again with the same parameter
-  this.data = data;
-  this.next = null;
-  this.previous = null;
-}  
+public class Node<T> { //generic  node T to let's as reuse it for task, and can use any datatype
+    public T data; // this is where we store, think of it as a box 
+    public Node<T> next; // pointer or reference to the next node of the chain 
+    public Node<T> previous; // pointer or reference to the previous node in the chain
+
+
+public Node(T data){ // contructor with the parameter of new thing, like it enables you to create a box with new thing 
+    this.data = data; // where we put the thing
+    this.next = null; // where we connect the box to the next box
+    this.previous = null; // where we connect the box to the past box
+
+}
 }
